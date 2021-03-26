faaaa131b0ddd2138eed6e62e3557a99693d9c6b
masahito1997 <velonica.gale2227@gmail.com>
2021-03-26 15:54:47 +0900

    implement first app whose name is "I Am Rich app"

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 大迫雅仁 on 2021/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
